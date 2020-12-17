a45576e2ad3e5f3a2543ad13b5fe1ccf68ad1487
Yeison Aguirre Osorio <naufragodev@gmail.com>
2020-12-17 15:05:51 -0500

    rutas de produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: true,
-  allCharacters: "http://hp-api.herokuapp.com/api/characters",
-  studentsAllCharacters:"http://hp-api.herokuapp.com/api/characters/students",
-  allCharactersBook:"http://hp-api.herokuapp.com/api/characters/staff",
-  allCharactersHouse: "http://hp-api.herokuapp.com/api/characters/house/"
+  api: "http://hp-api.herokuapp.com/api/characters",
+
 };
